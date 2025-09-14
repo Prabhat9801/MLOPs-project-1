9b7c740f175f961f781d9011053b6d0ca2b5e776
Prabhat9801 <prabhatkumarsictc7070@gmail.com>
2025-09-15 01:33:43 +0530

    data uploaded to mongoDB and the loging and exception file and therir code added and tested

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Prabhat Singh",
-    author_email="prabhatkumarsictc7070@gmail.com",
+    author="Vikash Das",
+    author_email="vikashdas770@gmail.com",
     packages=find_packages()
 )
