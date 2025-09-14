ed98206eff184011d4baef148b3273ec6091790f
Prabhat9801 <prabhatkumarsictc7070@gmail.com>
2025-09-15 00:59:52 +0530

    project structure ready , venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Prabhat Singh",
+    author_email="prabhatkumarsictc7070@gmail.com",
+    packages=find_packages()
+)
